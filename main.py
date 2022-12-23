3f2473d74b633a6a98f99917c861a68ce9315cb2
Ekaterina Komarova <borodatova@.bk.ru>
2022-12-23 22:26:53 +0400

    Add photo request

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ if __name__ == '__main__':
     # копирование фотографий из профиля VK
     id_user = input("Введите идентификатор пользователя VK или короткое имя: ")
     my_VK_profile = VK.VKUser(read_token('VK'), id_user, 'profile')
+    quan_photo = int(input("Введите количество копируемых фотографий профиля: "))
     with open('my_photos_VK_prof.json', 'w') as pfile:
         json.dump(my_VK_profile.jsonList, pfile)
-    my_YA = YDisk.Yandex('Photo VK_profile', read_token('Yandex'), 5)
+    my_YA = YDisk.Yandex('Photo VK_profile', read_token('Yandex'), quan_photo)
     my_YA.load_y_disk(my_VK_profile.exp_dict)
 
 #    копирование фотографий со стены VK
     my_VK_wall = VK.VKUser(read_token('VK'), id_user, 'wall')
+    quan_photo = int(input("Введите количество копируемых фотографий со стены: "))
     with open('my_photos_VK_saved.json', 'w') as wfile:
         json.dump(my_VK_wall.jsonList, wfile)
-    my_YA = YDisk.Yandex('Photo VK_wall', read_token('Yandex'), 15)
+    my_YA = YDisk.Yandex('Photo VK_wall', read_token('Yandex'), quan_photo)
     my_YA.load_y_disk(my_VK_wall.exp_dict)
